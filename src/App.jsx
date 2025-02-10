26d907be8632e2406d2a701e84878aeb5e7fd112
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2025-02-10 22:47:32 +0100

    next

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,7 @@ function App() {
           <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
           <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
 
-          <Sprej position={[0, 0, 0]} />
+          <Sprej position={[0, 0, 1]} />
 
           <SpriteAnimator
             position={[2, 0, 0]}
